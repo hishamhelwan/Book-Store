0fe68c14dd5180dd08fb1c20cc364888baf13a3a
hisham helwan <hishamhelwan@gmail.com>
2025-11-27 16:13:06 +0200

    1st commit

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
-//import { defineConfig } from 'vite'
- //import react from '@vitejs/plugin-react' 
-// export default defineConfig({
+import { defineConfig } from 'vite'
+ import react from '@vitejs/plugin-react' 
+ export default defineConfig({
 
- // plugins: [react()], 
+  plugins: [react()], 
 
- //})
- import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/Book-Store/',     // <–– Add this line (repo name + slashes)
-})
+ })
